3298b9cf4b19a87a3f25a5954a7eb662acd4f39a
Patrik Róbert Horváth <h.patrikrobert@gmail.com>
2023-11-12 17:13:38 +0100

    Delete Dogsta directory

--- Dogsta/server/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const uri = `mongodb://localhost:27017/events`;
-
-mongoose.connect(uri, 
-{ useNewUrlParser: true, useUnifiedTopology: true })
-.then( console.log('connected'))
-.catch(error => console.log(error));
-
-module.exports = mongoose;
